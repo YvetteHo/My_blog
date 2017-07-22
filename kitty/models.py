676ba12008f7b64553cada64e29b7639a7b3f4c4
YvetteHo <YvetteHo@126.com>
2017-07-22 22:34:32 +0800

    add body_text

--- kitty/models.py
+++ kitty/models.py
@@ -8,7 +8,7 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Post(models.Model):
     title_text = models.CharField(max_length=20)
-    body_text = models.TextField
+    body_text = models.TextField(None)
     publish_date = models.DateTimeField('data published')
 
     def __str__(self):
