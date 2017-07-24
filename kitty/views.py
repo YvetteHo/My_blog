f698ba9b91fd250c086f670d3b072c01c747ede8
YvetteHo <YvetteHo@126.com>
2017-07-24 12:02:42 +0800

    change homepage's view

--- kitty/views.py
+++ kitty/views.py
@@ -5,8 +5,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Post
+
 def homepage(request):
-    return HttpResponse("This is homepage")
+    latest_post_list = Post.objects.order_by('-publish_date')[:5]
+    output = ', '.join([p.title_text for p in latest_post_list])
+    return HttpResponse(output)
+
+def post(request):
+    return HttpResponse("Post")
 
 def about(request):
     return HttpResponse("about me")
