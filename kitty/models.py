dc84685fdfc351b0530d7ec7bee9f96348227df1
YvetteHo <YvetteHo@126.com>
2017-07-24 12:02:10 +0800

    add a subtitle_text to models.py

--- kitty/models.py
+++ kitty/models.py
@@ -8,11 +8,12 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Post(models.Model):
     title_text = models.CharField(max_length=20)
+    subtitle_text = models.CharField(max_length=20)
     body_text = models.TextField(None)
     publish_date = models.DateTimeField('data published')
 
     def __str__(self):
-        return str(self.title_text)+str(self.body_text)+str(self.publish_date)
+        return str(self.title_text)+str(self.subtitle_text)+str(self.publish_date)
 
     def was_published_recently(self):
         return self.publish_date >= timezone.now() - datetime.timedelta(days=1)
