e706c32109a262cd331f92cabee5136880a2afff
YvetteHo <YvetteHo@126.com>
2017-07-27 11:05:08 +0800

    change views

--- kitty/views.py
+++ kitty/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse
 
-from .models import Post
+from .models import Post, About
 
 from django.template import loader
 
@@ -17,22 +17,16 @@ def homepage(request):
     context = {'latest_post_list': latest_post_list}
     return render(request, 'kitty/homepage.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
 def post(request, url_text):
     post = get_object_or_404(Post, pk=url_text)
     return render(request, "kitty/post.html", {'post': post})
-    # try:
-    #     post = Post.objects.get(pk=url_text)
-    # except Post.DoesNotExist:
-    #     raise Http404("没有这篇博文的哦")
-    #
-    # return render(request, "kitty/post.html", {'post': post})
 
 def about(request):
-    return HttpResponse("about me")
+    try:
+        self_intro_text = About.objects.all().first()
+    except About.DoesNotExist:
+        self_intro_text = 'Nothing'
+    return render(request, 'kitty/about.html', {'self_intro_text': self_intro_text})
 
 def contact(request):
     return HttpResponse("contact me")
